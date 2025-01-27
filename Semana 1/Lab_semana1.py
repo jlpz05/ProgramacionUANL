00a71ad7584f2953535d3c190794d5f9fb1c85fa
Huntdown05 <57652486+jlpz05@users.noreply.github.com>
2025-01-27 03:07:54 +0000

    commit

---
+++ Semana 1/Lab_semana1.py
@@ -0,0 +1,58 @@
+print("Hola Mundo...")
+#Ejercicios Semana 1    
+
+#Ejercicio 1
+    #¿Cuál Es la funcion de la memoria secundaria en una computadora?
+        #c) Almacenar informacion durante mucho tiempo, incluso después de ciclos de apagado
+
+#Ejercicio 2
+    #¿Qué es un programa?
+        #R) Secuencia de declaraciones o sentencias que han sido diseñadas para hacer algo.
+
+#Ejercicio 3
+    #¿Cuál es la diferencia entre un copilador y un intérprete?
+        #R) Un copilador necesita que le entreguen el programa completo en un fichero mientras que el intérprete lee el código fuente de los programas como ha sido escrito lo analiza, e interpreta sus instrucciones sobre la marcha.
+
+#Ejercicio 4
+    #¿Cuál de los siguientes contiene "codigo máquina"?
+        #a) El interprete de Python
+
+#Ejercicio 5
+    #¿Qué está mal en el siguiente código?:
+    # primt ’¡Hola, mundo!’
+    #File "<stdin>", line 1
+    #primt ’¡Hola, mundo!’
+    #^
+    #SyntaxError: invalid syntax
+    #>>>
+
+        #R) Errores "gramaticales" print esta mal escrito y le faltan parentesis  
+
+#Ejercicio 6
+    #¿En qué lugar del computador queda almacenada una variable, como en este caso “x”, después de ejecutar la siguiente línea de Python?:
+    #x = 123
+        #a) Memoria principal
+
+#Ejercicio 7
+    #¿Qué mostrará en pantalla el siguiente programa?:
+    #x = 43
+    #x = x + 1
+    #print(x)      
+        #b) 44
+
+#Ejercicio 8
+    #Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:
+    #1. Unidad central de procesamiento.
+        #R) Cerebro 
+    #2. Memoria principal.
+        #R) Memoria a corto plazo, puede ser el hipocampo y los lobulos frontales los cuales también se encargan del pensamiento
+    #3. Memoria secundaria.
+        #R) El sistema límbico(hipocampo, amigdala, cerebelo) se encarga de la memoria a largo plazo
+    #4. Dispositivos de entrada.
+        #R) Los sentidos: olfato, tacto, vision, etc.
+    #5. Dispositivos de salida.
+        #R) El habla
+
+#Ejercicio 9
+    #¿Cómo puedes corregir un “Error de sintaxis”?
+        #R) Identificandolo y corrigiendo el error "gramatical", normalmente python señala la linea donde ocuurrio el error
