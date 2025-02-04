3f81d2316cff2208c1d455f7bb5b1bdd9389fdae
Huntdown05 <57652486+jlpz05@users.noreply.github.com>
2025-02-04 04:43:59 +0000

    commit

--- Semana 2/Problema 9.py
+++ Semana 2/Problema 9.py
@@ -1,8 +1,9 @@
 print("generar una lista de numeros pares e impares de hasta un limite dado")
 x=int(input("ingrese un numero: "))
 r=0
-y = []
+y = []#una lista vacia
 while r<x:
     r=r+1
-    y.append(r)
-print(y)
+    y.append(r)#añade el numero a la lista
+print(y)#imprime la lista
+#mi unico problema fue que puse la declaracion de la lista en el while, entonces se borraba 
